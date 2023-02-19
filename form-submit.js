2ab8db9c6f6ab76630407c2c13583ffb4af8ca4a
Gokul Muthukumar <gokul.muthukumar@digitalsoft.com>
2023-02-19 10:03:17 +0530

    init commit

---
+++ form-submit.js
@@ -0,0 +1,28 @@
+// window.onload = function () {
+
+//     const learnMoreButtons = document.querySelectorAll('.learn-more');
+//     const modal = document.getElementById('modal');
+//     const closeButton = document.querySelector('.close-button');
+//     learnMoreButtons.forEach(button => {
+//         button.addEventListener('click', e => {
+//             modal.style.display = 'block';
+//         });
+//     });
+
+//     closeButton.addEventListener('click', e => {
+//         modal.style.display = 'none';
+//     });
+    
+
+// }
+// $(document).ready(function() {
+//     $(".learn-more").click(function() {
+//       var popupId = $(this).data("popup-id");
+//       $("#" + popupId).show();
+//     });
+  
+//     $(".modal .close-button").click(function() {
+//       $(this).closest(".popup").hide();
+//     });
+//   });
+
